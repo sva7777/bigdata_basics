d5128e350a6546e1e7fcd53917643bf722c416e2
sva7777 <sva7777@gmail.com>
2023-01-01 01:19:56 +0300

    add speed test result extraction

---
+++ 2.5.1.4/main.py
@@ -0,0 +1,31 @@
+import datetime
+import csv
+import pandas as pd
+import numpy as np
+
+data_file = './Data/rpi_data_long.csv'
+
+column_names = [ 'Type A', 'Measure A', 'Units A',
+                 'Type B', 'Measure B', 'Units B',
+                 'Type C', 'Measure C', 'Units C',
+                 'Datetime']
+with open(data_file, 'r') as f:
+    df_redundant = pd.read_csv(f, names = column_names)
+
+df_compact = df_redundant.copy()
+df_compact.rename(columns={'Measure A':'Ping (ms)',
+                           'Measure B': 'Download (Mbit/s)',
+                           'Measure C': 'Upload (Mbit/s)'}, inplace=True)
+
+df_compact.drop(['Type A', 'Type B', 'Type C',
+         'Units A', 'Units B', 'Units C'], axis=1, inplace=True)
+df_compact.head()
+
+df_compact['Date'] = df_compact['Datetime'].apply(lambda dt_str: pd.to_datetime(dt_str).date())
+temp = df_compact['Datetime'].apply(lambda dt_str: pd.to_datetime(dt_str))
+df_compact['Time'] = temp.dt.time
+
+del df_compact['Datetime']
+
+df_compact.to_csv('./Data/rpi_data_compact.csv')
+
---
+++ 2.5.1.4/prepear_data.py
@@ -0,0 +1,39 @@
+import datetime
+import csv
+import subprocess
+
+
+def speedtest():
+    date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    speedtest_cmd = "speedtest-cli --simple"
+    process = subprocess.Popen(speedtest_cmd.split(), stdout=subprocess.PIPE)
+    process_output = process.communicate()[0]
+    process_output = process_output.decode('utf-8')
+    process_output = process_output.split()
+    process_output.append(date_time)
+    return process_output
+
+def save_to_csv(data, filename):
+    try:
+        with open(filename + '.csv', 'a') as f:
+            wr = csv.writer(f)
+            wr.writerow(data)
+    except:
+        with open(filename + '.csv', 'w') as f:
+            wr = csv.writer(f)
+            wr.writerow(data)
+
+def print_from_csv(filename):
+    with open(filename + '.csv', 'r') as f:
+            re = csv.reader(f)
+            for line in re:
+                print(line)
+if __name__ == '__main__':
+
+#    for i in range(5):
+#        speedtest_output = speedtest()
+#        print('Test number {}'.format(i))
+#        print(speedtest_output)
+#        save_to_csv(speedtest_output, '/tmp/rpi_data_test')
+    print_from_csv('/tmp/rpi_data_test')
+
